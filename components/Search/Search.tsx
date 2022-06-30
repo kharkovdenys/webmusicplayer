47bb456eefb24304b464d80a93dbfd3962680ce4
Харьков Денис Вячеславович <kharkov_dv@oit.dp.ua>
2022-06-30 16:32:30 +0200

    Add support svg

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -3,10 +3,11 @@ import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
 import styles from "./Search.module.css";
 import cn from "classnames";
+import SearchIcon from "./search.svg";
 
 export const Search = ({ className, placeholder, ...props }: SearchProps): JSX.Element => {
     return <form className={cn(className, styles.search)} autoComplete={"off"} role={"search"} {...props} >
         <Input placeholder={placeholder} name={"search_music"} type={"text"} />
-        <Button>Search</Button>
+        <Button><SearchIcon width={"24px"} height={"24px"} /></Button>
     </form>;
 };
