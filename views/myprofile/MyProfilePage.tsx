6d9fa3ff8bdf1ccf12e97e8b4441add056d391a2
Харьков Денис Вячеславович <kharkov_dv@oit.dp.ua>
2022-07-10 18:55:19 +0200

    Fix MyProfilePage

--- views/myprofile/MyProfilePage.tsx
+++ views/myprofile/MyProfilePage.tsx
@@ -1,32 +1,41 @@
 import axios from "axios";
 import { deleteCookie, getCookie } from "cookies-next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { Avatar, Button, CircularProgress } from "../../components";
+import { Avatar, Banner, Button, CircularProgress } from "../../components";
 
 export const MyProfilePage = (): JSX.Element => {
     const [name, setName] = useState("");
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
     const navigate = useNavigate();
-    axios.get("https://databaseandapi.azurewebsites.net/info", { headers: { Authorization: getCookie("token") ?? "" } }).then((name) => { setName(name.data); setLoading(true); });
+    useEffect(() => {
+        setError(false);
+        axios.get("https://databaseandapi.azurewebsites.net/info", { headers: { Authorization: getCookie("token") ?? "" } }).then((name) => {
+            setName(name.data); setLoading(true);
+        }).catch(() => { setLoading(true); setError(true); });
+    }, []);
     return (
         <div style={{ marginLeft: "auto", marginRight: "auto", alignItems: "center", justifyItems: "center", display: "flex", flexDirection: "column" }}>
-            {loading ? <><Avatar style={{ marginTop: "20px" }}>
-                {name[0]}
-            </Avatar>
-                <p
-                    style={{
-                        fontSize: "32px",
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                        marginTop: "20px",
-                        marginBottom: "20px",
-                        textAlign: "center",
-                    }}
-                >
-                    {name}
-                </p></> : <CircularProgress style={{ marginTop: "43.5px", marginBottom: "43.5px" }} />}
-            <NavLink to="/" style={{ textDecoration: "none" }}>
+            {loading ? getCookie("token") === undefined ? <Banner>😑 Oops.. You are not logged in</Banner> :
+                error ? <Banner>😑 Oops.. Something went wrong</Banner> :
+                    <><Avatar style={{ marginTop: "20px" }}>
+                        {name[0]}
+                    </Avatar>
+                        <p
+                            style={{
+                                fontSize: "32px",
+                                marginLeft: "auto",
+                                marginRight: "auto",
+                                marginTop: "20px",
+                                marginBottom: "20px",
+                                textAlign: "center",
+                            }}
+                        >
+                            {name}
+                        </p></> : <CircularProgress style={{ marginTop: "43.5px", marginBottom: "43.5px" }} />
+            }
+            <NavLink to="/" style={{ textDecoration: "none", marginTop: "10px" }}>
                 <Button variant="out" onClick={(): void => { deleteCookie("token"); navigate("/"); }}>
                     Sing Out
                 </Button>
